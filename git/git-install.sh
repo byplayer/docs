2967c23ef10b10ba0d6caaf15c7ec01263ad6623
yukio <yukio@f9cb7afc-d326-6f48-a1b1-0700e66b032a>
2010-01-05 04:28:26 +0000

    git install用シェルスクリプト追加
    git-svn-id: http://www.emaki.minidns.net/svn/projects/docs@612 f9cb7afc-d326-6f48-a1b1-0700e66b032a

---
+++ git/git-install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# This script run as root
+# -----------------------------------------------------------
+#  事前準備
+# -----------------------------------------------------------
+# aptitude install gettext tcl tk libsvn-perl asciidoc libcurl4-openssl-dev curl
+#
+GIT_VER=1.6.6
+
+cd /usr/local/src
+
+
+if [ ! -f git-${GIT_VER}.tar.bz2 ];
+then
+  wget http://kernel.org/pub/software/scm/git/git-${GIT_VER}.tar.bz2
+fi
+
+
+
+tar xjf git-${GIT_VER}.tar.bz2
+
+cd git-${GIT_VER}
+
+
+
+./configure --with-openssl --with-curl --with-expat --with-perl --prefix=/usr/local/git-${GIT_VER}
+
+
+# debian 4の場合は
+# vi http.h
+
+# 以下をコメントアウト
+
+# -----------------------------------------------------------
+#if LIBCURL_VERSION_NUM >= 0x071000
+...
+#endif
+# -----------------------------------------------------------
+
+make
+make man
+make install
+make install-man
+
+
+pushd /usr/local
+if [ -f git ];
+then
+  rm git
+fi
+ln -s git-${GIT_VER} git
+popd
+
+# perl の SVNパッケージを入れておかないと
+# git svnが使えない
+
+# git 環境設定
+# export PATH=/usr/local/git/bin:$PATH
+# export MANPATH=/usr/local/git/share/man:`manpath -q`
