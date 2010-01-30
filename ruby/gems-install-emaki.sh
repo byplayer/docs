f01ca14646d0913b12d4224acd9d19acabfc54a7
yukio <yukio@f9cb7afc-d326-6f48-a1b1-0700e66b032a>
2010-01-30 04:57:43 +0000

    emakiホスト向けのgemインストールスクリプト追加
    git-svn-id: http://www.emaki.minidns.net/svn/projects/docs@617 f9cb7afc-d326-6f48-a1b1-0700e66b032a

---
+++ ruby/gems-install-emaki.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# aptitude install libxml2-dev libxslt-dev
+
+which ruby
+which gem
+ruby -v
+gem -v
+gem env
+
+gem install bluecloth
+gem install builder
+gem install capistrano
+gem install cucumber
+gem install fastthread
+gem install gemcutter
+gem install git
+gem install highline
+gem install hoe
+gem install jeweler
+gem install json_pure
+gem install nokogiri
+gem install passenger
+gem install polyglot
+gem install racc
+gem install rack
+gem install rails
+gem install rake
+gem install rake-compiler
+gem install rexical
+gem install rr
+gem install rspec
+gem install rspec-rails
+gem install rubyforge
+gem install rwsc
+gem install term-ansicolor
+gem install treetop
+gem install webrat
