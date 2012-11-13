97e135f150cfde09454a03a57ff3de7ee869a058
byplayer <byplayer100@gmail.com>
2012-11-13 16:39:57 +0900

    add install script for global

---
+++ linux/install/install_global.sh
@@ -0,0 +1,15 @@
+cd /usr/local/src
+
+GB_VER=6.2.4
+wget http://tamacom.com/global/global-${GB_VER}.tar.gz
+
+tar xzf global-${GB_VER}.tar.gz
+
+cd global-${GB_VER}
+./configure --prefix=/usr/local/global-${GB_VER}
+
+make
+make install
+
+cd /usr/local/
+ln -s global-${GB_VER} global
