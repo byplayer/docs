f5220fbe29d7f947c175b56a4134c9080e09dd7c
byplayer <byplayer100@gmail.com>
2014-11-06 10:47:54 +0800

    update git version

--- git/git-install.sh
+++ git/git-install.sh
@@ -7,7 +7,7 @@
 aptitude install -y gettext tcl tk libsvn-perl asciidoc libcurl4-dev libcurl4-openssl-dev curl\
   libexpat-dev zlib1g-dev libbz2-dev
 #
-GIT_VER=2.1.0
+GIT_VER=2.1.3
 
 cd /usr/local/src
 
