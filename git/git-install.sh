28eca85c1b852f64aec52ea84e387f34aa454fbb
Yukio Goto <yukio.goto@mail.rakuten.com>
2015-01-30 10:18:48 +0800

    update version and install dir

--- git/git-install.sh
+++ git/git-install.sh
@@ -6,41 +6,46 @@
 # -----------------------------------------------------------
 aptitude install -y gettext tcl tk libsvn-perl asciidoc libcurl4-dev libcurl4-openssl-dev curl\
   libexpat-dev zlib1g-dev libbz2-dev
-#
-GIT_VER=2.2.1
+
+GIT_VER=2.2.2
+INSTALL_DIR=/opt/git
+
+${INSTALL_DIR}/bin/git --version 2> /dev/null | grep ${GIT_VER} > /dev/null 2>&1
+result=$?
+if [ $result -eq 0 ]; then
+  echo git ${GIT_VER} is already installed
+  exit 0
+fi
+
+set -e
+
+if [ -d ${INSTALL_DIR} ]; then
+    rm -rf ${INSTALL_DIR}
+fi
 
 cd /usr/local/src
 
-if [ ! -d /opt/git-${GIT_VER} ]; then
-  if [ ! -f git-${GIT_VER}.tar.gz ]; then
+if [ ! -f git-${GIT_VER}.tar.gz ]; then
     wget https://www.kernel.org/pub/software/scm/git/git-${GIT_VER}.tar.gz
-  fi
-  
-  if [ -d git-${GIT_VER} ]; then
+fi
+
+if [ -d git-${GIT_VER} ]; then
     rm -rf git-${GIT_VER}
-  fi
-
-  tar xzf git-${GIT_VER}.tar.gz
-
-  cd git-${GIT_VER}
-
-  ./configure --with-openssl --with-curl --with-expat --with-perl --prefix=/opt/git-${GIT_VER}
-
-
-  make
-  make install
-  
-  make man
-  make install-man
 fi
 
-pushd /opt
-if [ -L git ];
-then
-  rm git
-fi
-ln -s git-${GIT_VER} git
-popd
+tar xzf git-${GIT_VER}.tar.gz
+
+cd git-${GIT_VER}
+
+./configure --with-openssl --with-curl --with-expat --with-perl --prefix=${INSTALL_DIR}
+
+
+make
+make install
+
+make man
+make install-man
+
 
 # If you want to use git svn, you need to install perl svn .
 
