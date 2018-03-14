a2f9f00913c8bc128415a4f57d6d72abda804075
byplayer <byplayer100@gmail.com>
2018-03-14 16:54:02 +0800

    update git version

--- git/git-install.sh
+++ git/git-install.sh
@@ -10,7 +10,7 @@ set -e
 apt -y install gettext tcl tk libsvn-perl asciidoc libcurl4-openssl-dev curl\
   libexpat-dev zlib1g-dev libbz2-dev
 
-GIT_VER=2.16.1
+GIT_VER=2.16.2
 INSTALL_DIR=/opt/git
 
 set +e
