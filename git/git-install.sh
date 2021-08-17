35dd8988e973b04e3d88f23b9770795fd9352192
byplayer <byplayer100@gmail.com>
2021-08-17 15:44:32 +0900

    bump up git version

--- git/git-install.sh
+++ git/git-install.sh
@@ -10,7 +10,7 @@ set -e
 apt -y install gettext make tcl tk libsvn-perl asciidoc libcurl4-openssl-dev curl\
   libexpat-dev zlib1g-dev libbz2-dev
 
-GIT_VER=2.32.0
+GIT_VER=2.33.0
 INSTALL_DIR=/opt/git
 
 set +e
